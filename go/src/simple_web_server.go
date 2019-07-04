31e25eacfe701b91af55bab3a07dd03c966e3384
phoenixcoder <thephoenixcoder@gmail.com>
2019-07-03 19:49:00 -0700

    Add compute bomb to golang server.

--- go/src/simple_web_server.go
+++ go/src/simple_web_server.go
@@ -4,13 +4,23 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 )
 
 var msg string = "Hello, I'm your webserver today!!! My name is: %s.\n"
 
+func computeBomb() {
+	for i := 0; i < 1000; i++ {
+		randF1 := rand.Float64()
+		randF2 := rand.Float64()
+		randF2 = (randF1 * randF2) - randF2
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
+	computeBomb()
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	io.WriteString(w, msg)
 }
