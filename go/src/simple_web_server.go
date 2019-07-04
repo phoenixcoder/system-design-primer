d081dad172235e94c25ca065d2d7f45eae3a238b
phoenixcoder <thephoenixcoder@gmail.com>
2019-07-03 21:06:55 -0700

    Update Simple Web Server
    * Add sleep time parameter.
    * Add cycles parameter.

--- go/src/simple_web_server.go
+++ go/src/simple_web_server.go
@@ -7,12 +7,14 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 )
 
-var msg string = "Hello, I'm your webserver today!!! My name is: %s.\n"
+var serverName string = ""
 
-func computeBomb() {
-	for i := 0; i < 1000; i++ {
+func computeBomb(cycles int) {
+	for i := 0; i < cycles; i++ {
 		randF1 := rand.Float64()
 		randF2 := rand.Float64()
 		randF2 = (randF1 * randF2) - randF2
@@ -20,7 +22,41 @@ func computeBomb() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	computeBomb()
+	msg := "Hello, I'm your webserver today!!!\nMy name is, %s.\n"
+	query := r.URL.Query()
+	sleepTime := 0
+	sleepTimeStr := ""
+	cycles := 0
+	cyclesStr := ""
+
+	if serverName == "" {
+		msg = fmt.Sprintf(msg, "uhhhh...yeah, I forgot.")
+	} else {
+		msg = fmt.Sprintf(msg, serverName)
+	}
+
+	if sleepTimeStr = query.Get("sleepTime"); sleepTimeStr != "" {
+		sleepTimeC, err := strconv.Atoi(sleepTimeStr)
+		sleepTime = sleepTimeC
+		if sleepTime < 0 || err != nil {
+			log.Print("sleepTime Err non-null")
+			sleepTime = 0
+		}
+	}
+
+	if cyclesStr = query.Get("cycles"); cyclesStr != "" {
+		cyclesC, err := strconv.Atoi(cyclesStr)
+		cycles = cyclesC
+		if cycles < 0 || err != nil {
+			cycles = 0
+		}
+	}
+
+	computeBomb(cycles)
+	time.Sleep(time.Duration(sleepTime) * time.Millisecond)
+	msg += "I've slept " + strconv.Itoa(sleepTime) + " milliseconds.\n"
+	msg += "I've computed " + strconv.Itoa(cycles) + " times.\n"
+	msg += "Goodbye!\n"
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	io.WriteString(w, msg)
 }
@@ -33,12 +69,7 @@ func getServerName(args []string) string {
 }
 
 func main() {
-	serverName := getServerName(os.Args[1:])
-	if serverName == "" {
-		msg = fmt.Sprintf(msg, "uhhhh...yeah, I forgot.")
-	} else {
-		msg = fmt.Sprintf(msg, serverName)
-	}
-	http.HandleFunc("/", handler)
+	serverName = getServerName(os.Args[1:])
+	http.HandleFunc("/request", handler)
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
